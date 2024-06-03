53fec29e9859db675c6673d53a8879b667d37ddf
Wera37 <138698500+Wera37@users.noreply.github.com>
2024-06-03 11:59:42 +0800

    meet 9 ipul

---
+++ struktur data/mett9.py
@@ -0,0 +1,26 @@
+# percabangan jika maka
+
+name = input ("what is yor name :")
+print (name)
+
+# if kondisi : aksi
+
+# if standar
+
+if name == "ivule" : print ("driver muda")
+print ("end of progrfam")
+
+
+# if identitas
+if name =="ivule" :
+    print ("driver muda")
+    print ("muda banget")
+print ("end of program")
+
+# if else
+if name =="ivule" :
+    print ("driver muda")
+    print (f"terima kasih {name}")
+else:
+    print ("maaf anda terlalu tua")
+print ("end of program")
---
+++ struktur data/pert7.py
@@ -0,0 +1,49 @@
+# tabel kebenaran (logika bolean)
+# not and or xor
+
+# not
+print("******logika NOT******")
+x = False
+y = not x
+print('value x =', x)
+print('**********NOT')
+print('velue y=',y)
+
+# OR (semua bernilai true asalkan ada truenya)
+print('*********logika OR******')
+X = False
+y = False
+z = x or y
+print(x, 'OR' , y, ' 
+      
+  # perbendingan lanjutan 
+
+# +++++ 3 -------- 9 +++++
+MasukanUser = float (input ("masukan bilangan \nkurang dari 3 atau lebih dari 9 : " )) 
+
+# ----- cek kurang dari 3
+kurDar = (MasukanUser < 3 ) 
+print (" kurang dari 3 =", kurDar )
+
+# ----- cek lebih  dari 9
+lebDar = (MasukanUser > 9)
+print (" lebih dari 9 =", lebDar )
+
+betul = kurDar or lebDar 
+print ( "pengecekan final : ", betul  )
+
+# 4 14 
+MasukanUser = float (input ("masukan bilangan antara 4 dan 14 : ") ) 
+hasil = ( 4 < MasukanUser < 14 ) 
+print ( hasil )
+
+# cek lebih dari 10 
+lebDar = ( MasukanUser > 4)
+print ( " lebih dari 4 =" , lebDar )
+
+# betul 
+betul = kurDar or lebDar 
+print (" pengecekan final :", betul)
+hasil =(4 < MasukanUser < 14)
+print (hasil) 
+S   
